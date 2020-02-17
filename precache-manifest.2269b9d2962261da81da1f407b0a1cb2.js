359c6f57ae99b2658c268950324115ee8e6c2858
Blake <bryantb2@my.lanecc.edu>
2020-02-16 20:22:15 -0800

    Updates

---
+++ precache-manifest.2269b9d2962261da81da1f407b0a1cb2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031586722060992ddfc49a6bcd767d41",
+    "url": "/OPES-FREEDOM/index.html"
+  },
+  {
+    "revision": "44be40092d35d43c2ae4",
+    "url": "/OPES-FREEDOM/static/css/main.d26f65ff.chunk.css"
+  },
+  {
+    "revision": "95369598fa3df1626848",
+    "url": "/OPES-FREEDOM/static/js/2.6df23658.chunk.js"
+  },
+  {
+    "revision": "fabe6e8bc1203e177b62cd9daebbf511",
+    "url": "/OPES-FREEDOM/static/js/2.6df23658.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44be40092d35d43c2ae4",
+    "url": "/OPES-FREEDOM/static/js/main.dc5d5b60.chunk.js"
+  },
+  {
+    "revision": "3a36449c15b25372f575",
+    "url": "/OPES-FREEDOM/static/js/runtime-main.1fc4ecef.js"
+  },
+  {
+    "revision": "56f8bd4e0b54c16ac9a091ca12fb31c1",
+    "url": "/OPES-FREEDOM/static/media/arBackgroundRef.56f8bd4e.jpg"
+  },
+  {
+    "revision": "339d239c4bfb0d3263cac0dd10e0b3e0",
+    "url": "/OPES-FREEDOM/static/media/arStoreRef.339d239c.jpg"
+  },
+  {
+    "revision": "21064a36db49c68f5385e1cfcca0fed8",
+    "url": "/OPES-FREEDOM/static/media/bitcoinRef.21064a36.jpg"
+  },
+  {
+    "revision": "429765d847d770f86f49dfd41ba92689",
+    "url": "/OPES-FREEDOM/static/media/blonde2StoreRef.429765d8.jpg"
+  },
+  {
+    "revision": "4fee1bd0e46ee21eb2da929cfab3bcee",
+    "url": "/OPES-FREEDOM/static/media/brccIconRef.4fee1bd0.jpg"
+  },
+  {
+    "revision": "69129fc888c408944e15c1110d71e6ed",
+    "url": "/OPES-FREEDOM/static/media/clothingRef.69129fc8.jpg"
+  },
+  {
+    "revision": "090beac87400f5919631c9811cbaa3a3",
+    "url": "/OPES-FREEDOM/static/media/dailyWireRef.090beac8.png"
+  },
+  {
+    "revision": "606ae5cfc377b165c6e804b9161e8215",
+    "url": "/OPES-FREEDOM/static/media/energyDrinkRef.606ae5cf.jpg"
+  },
+  {
+    "revision": "0cd1acad22b5da067d9e1c14be449555",
+    "url": "/OPES-FREEDOM/static/media/energyDrinkStoreRef.0cd1acad.jpg"
+  },
+  {
+    "revision": "8e050585ed5d86d248f275464d2d2c56",
+    "url": "/OPES-FREEDOM/static/media/firearmPartsRef.8e050585.jpg"
+  },
+  {
+    "revision": "56692c08436f029289c9b48896031674",
+    "url": "/OPES-FREEDOM/static/media/goaRef.56692c08.png"
+  },
+  {
+    "revision": "ffbfbb9a6bb48e14ea07e79fda9dc430",
+    "url": "/OPES-FREEDOM/static/media/magaStoreRef.ffbfbb9a.jpg"
+  },
+  {
+    "revision": "690fe6ae4d79c0ba49e5d0d72cb0a8dd",
+    "url": "/OPES-FREEDOM/static/media/mwClothingRef.690fe6ae.jpg"
+  },
+  {
+    "revision": "62f0c16533538bfeead80a2292fa4fcd",
+    "url": "/OPES-FREEDOM/static/media/podcastRef.62f0c165.jpg"
+  },
+  {
+    "revision": "a72dacdfec00fc78cfddedc309c41543",
+    "url": "/OPES-FREEDOM/static/media/trumpMAGARef.a72dacdf.jpg"
+  },
+  {
+    "revision": "55deb4806e759fa2f3b1c331b7e0587e",
+    "url": "/OPES-FREEDOM/static/media/usMilitarySearchPage.55deb480.jpg"
+  },
+  {
+    "revision": "5613092dd697b4bca3eb383531e3eb66",
+    "url": "/OPES-FREEDOM/static/media/whiteTransparent.5613092d.png"
+  }
+]);
